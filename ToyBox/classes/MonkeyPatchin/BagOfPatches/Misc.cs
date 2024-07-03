308ca4de0ef590d6bf7ac278921e86688cc7ad32
xADDBx <62178123+xADDBx@users.noreply.github.com>
2024-07-03 08:38:05 +0200

    Logging

--- ToyBox/classes/MonkeyPatchin/BagOfPatches/Misc.cs
+++ ToyBox/classes/MonkeyPatchin/BagOfPatches/Misc.cs
@@ -4,6 +4,7 @@ using Kingmaker.QA;
 using ModKit;
 using Owlcat.Runtime.Core.Logging;
 using System;
+using static UnityModManagerNet.UnityModManager;
 
 namespace ToyBox.BagOfPatches {
     internal static partial class Misc {
@@ -17,9 +18,11 @@ namespace ToyBox.BagOfPatches {
         [HarmonyPatch(typeof(LogChannelEx), nameof(LogChannelEx.ErrorWithReport), [typeof(LogChannel), typeof(string), typeof(object[])])]
         private static class PatchyPatch {
             [HarmonyFinalizer]
-            private static Exception NoErrorInLoggingPlease(Exception __exception) {
+            private static Exception NoErrorInLoggingPlease(Exception __exception, string msgFormat, params object[] @params) {
                 if (__exception != null) {
                     Mod.Log(__exception?.ToString() ?? "");
+                    string text = string.Format(msgFormat, @params);
+                    Mod.Warn(text);
                 }
                 return null;
             }
