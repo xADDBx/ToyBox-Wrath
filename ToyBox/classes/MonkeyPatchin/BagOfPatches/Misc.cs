a8ad4930fa5a6a02c507c8c67f4de09cf3f92325
xADDBx <62178123+xADDBx@users.noreply.github.com>
2024-08-19 03:41:54 +0200

    .

--- ToyBox/classes/MonkeyPatchin/BagOfPatches/Misc.cs
+++ ToyBox/classes/MonkeyPatchin/BagOfPatches/Misc.cs
@@ -7,21 +7,21 @@ using System;
 
 namespace ToyBox.BagOfPatches {
     internal static partial class Misc {
+        [HarmonyPatch(typeof(LogChannelEx), nameof(LogChannelEx.ErrorWithReport), [typeof(LogChannel), typeof(string), typeof(object[])])]
+        private static class LogChannelEx_ErrorWithReport_Patch {
+            [HarmonyFinalizer]
+            private static Exception ErrorWithReport(Exception __exception) {
+                if (__exception != null) {
+                    Mod.Log(__exception?.ToString() ?? "");
+                }
+                return null;
+            }
+        }
         [HarmonyPatch(typeof(AchievementsManager), nameof(AchievementsManager.OnAchievementUnlocked))]
         private static class AchievementsManager_OnAchievementsUnlocked_Patch {
             private static void Postfix(AchievementEntity ach) {
                 AchievementsUnlocker.unlocked.Add(ach);
                 AchievementsUnlocker.AchievementBrowser.needsReloadData = true;
-            }
-        }
-        [HarmonyPatch(typeof(LogChannelEx), nameof(LogChannelEx.ErrorWithReport), [typeof(LogChannel), typeof(string), typeof(object[])])]
-        private static class PatchyPatch {
-            [HarmonyFinalizer]
-            private static Exception NoErrorInLoggingPlease(Exception __exception) {
-                if (__exception != null) {
-                    Mod.Log(__exception?.ToString() ?? "");
-                }
-                return null;
             }
         }
     }
