1bb27764a636e07fa50a4f4679fd2ea9cc5b1795
xADDBx <62178123+xADDBx@users.noreply.github.com>
2024-09-16 11:39:47 +0200

    Backport extension

--- ToyBox/Classes/ModKit/Utility/Extensions/MiscExtensions.cs
+++ ToyBox/Classes/ModKit/Utility/Extensions/MiscExtensions.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Unity.Collections;
+using UnityEngine.Rendering;
+using UnityEngine;
 
 namespace ModKit.Utility.Extensions {
     public static class MiscExtensions {
@@ -44,6 +47,66 @@ namespace ModKit.Utility.Extensions {
             }
 
             return obj.ToString();
+        }
+        public enum SaveTextureFileFormat {
+            PNG,
+            JPG,
+            EXR,
+            TGA
+        }
+        public static void SaveTextureToFile(this Texture source, string filePath, int width, int height, SaveTextureFileFormat fileFormat = SaveTextureFileFormat.PNG, int jpgQuality = 95, bool asynchronous = true, System.Action<bool> done = null) {
+            // check that the input we're getting is something we can handle:
+            if (!(source is Texture2D || source is RenderTexture)) {
+                done?.Invoke(false);
+                return;
+            }
+
+            // use the original texture size in case the input is negative:
+            if (width < 0 || height < 0) {
+                width = source.width;
+                height = source.height;
+            }
+
+            // resize the original image:
+            var resizeRT = RenderTexture.GetTemporary(width, height, 0);
+            Graphics.Blit(source, resizeRT);
+
+            // create a native array to receive data from the GPU:
+            var narray = new NativeArray<byte>(width * height * 4, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+
+            // request the texture data back from the GPU:
+            var request = AsyncGPUReadback.RequestIntoNativeArray(ref narray, resizeRT, 0, (AsyncGPUReadbackRequest request) => {
+                // if the readback was successful, encode and write the results to disk
+                if (!request.hasError) {
+                    NativeArray<byte> encoded;
+
+                    switch (fileFormat) {
+                        case SaveTextureFileFormat.EXR:
+                            encoded = ImageConversion.EncodeNativeArrayToEXR(narray, resizeRT.graphicsFormat, (uint)width, (uint)height);
+                            break;
+                        case SaveTextureFileFormat.JPG:
+                            encoded = ImageConversion.EncodeNativeArrayToJPG(narray, resizeRT.graphicsFormat, (uint)width, (uint)height, 0, jpgQuality);
+                            break;
+                        case SaveTextureFileFormat.TGA:
+                            encoded = ImageConversion.EncodeNativeArrayToTGA(narray, resizeRT.graphicsFormat, (uint)width, (uint)height);
+                            break;
+                        default:
+                            encoded = ImageConversion.EncodeNativeArrayToPNG(narray, resizeRT.graphicsFormat, (uint)width, (uint)height);
+                            break;
+                    }
+
+                    System.IO.File.WriteAllBytes(filePath, encoded.ToArray());
+                    encoded.Dispose();
+                }
+
+                narray.Dispose();
+
+                // notify the user that the operation is done, and its outcome.
+                done?.Invoke(!request.hasError);
+            });
+
+            if (!asynchronous)
+                request.WaitForCompletion();
         }
     }
 }
