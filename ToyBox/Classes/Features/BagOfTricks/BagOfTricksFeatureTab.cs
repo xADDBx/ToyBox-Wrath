485d904e03a3b94313cdc5f794ec96e81b73b566
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-04-05 23:23:07 +0200

    Implement groupings

--- ToyBox/Classes/Features/BagOfTricks/BagOfTricksFeatureTab.cs
+++ ToyBox/Classes/Features/BagOfTricks/BagOfTricksFeatureTab.cs
@@ -1,15 +1,38 @@
-﻿namespace ToyBox.Features.BagOfTricks;
+﻿using ToyBox.Infrastructure.UI;
+using UnityEngine;
+
+namespace ToyBox.Features.BagOfTricks;
 public partial class BagOfTricksFeatureTab : FeatureTab {
     [LocalizedString("ToyBox_Features_BagOfTricks_BagOfTricksFeatureTab_BagOfTricksText", "Bag of Tricks")]
     public override partial string Name { get; }
+    [LocalizedString("ToyBox_Features_BagOfTricks_BagOfTricksFeatureTab_QualityOfLifeText", "Quality of Life")]
+    private static partial string QualityOfLifeText { get; }
+    [LocalizedString("ToyBox_Features_BagOfTricks_BagOfTricksFeatureTab_CheatsText", "Cheats")]
+    private static partial string CheatsText { get; }
     public BagOfTricksFeatureTab() {
-        AddFeature(new EnableAchievementsFeature());
-        AddFeature(new SpontaneousCasterCopyScrollFeature());
-        AddFeature(new DisableFogOfWarFeature());
-        AddFeature(new RestAfterCombatFeature());
-        AddFeature(new RestoreAbilitiesAfterCombatFeature());
-        AddFeature(new RestoreSpellsAfterCombatFeature());
-        AddFeature(new RestoreItemsAfterCombatFeature());
-        AddFeature(new ToggleLockJamFeature());
+        AddFeature(new EnableAchievementsFeature(), QualityOfLifeText);
+        AddFeature(new SpontaneousCasterCopyScrollFeature(), CheatsText);
+        AddFeature(new DisableFogOfWarFeature(), CheatsText);
+        AddFeature(new RestAfterCombatFeature(), CheatsText);
+        AddFeature(new RestoreAbilitiesAfterCombatFeature(), CheatsText);
+        AddFeature(new RestoreSpellsAfterCombatFeature(), CheatsText);
+        AddFeature(new RestoreItemsAfterCombatFeature(), CheatsText);
+        AddFeature(new ToggleLockJamFeature(), CheatsText);
+    }
+    public override void OnGui() {
+        foreach (var (groupName, features) in GetGroups()) {
+            using (VerticalScope()) {
+                GUILayout.Label(groupName, GUILayout.ExpandWidth(false));
+                using (HorizontalScope()) {
+                    GUILayout.Space(25);
+                    using (VerticalScope()) {
+                        foreach (var feature in features) {
+                            feature.OnGui();
+                        }
+                    }
+                }
+            }
+            Div.DrawDiv();
+        }
     }
 }
--- ToyBox/Classes/Infrastructure/Features/FeatureTab.cs
+++ ToyBox/Classes/Infrastructure/Features/FeatureTab.cs
@@ -3,18 +3,33 @@ using ToyBox.Infrastructure.UI;
 
 namespace ToyBox;
 public abstract class FeatureTab {
-    private List<Feature> Features { get; set; } = new();
+    private Dictionary<string, List<Feature>> FeatureGroups { get; set; } = new();
     public abstract string Name { get; }
-    public virtual void AddFeature(Feature feature) {
+    public virtual void AddFeature(Feature feature, string groupName = "") {
         if (feature is INeedEarlyInitFeature) {
             feature.Initialize();
         }
-        Features.Add(feature);
+        if (!FeatureGroups.TryGetValue(groupName, out var group)) {
+            group = new();
+            FeatureGroups[groupName] = group;
+        }
+        group.Add(feature);
     }
-    public IEnumerable<Feature> GetFeatures() => Features;
+    public IEnumerable<Feature> GetFeatures() {
+        foreach (var group in FeatureGroups.Values) {
+            foreach (var feature in group) {
+                yield return feature;
+            }
+        }
+    }
+    public IEnumerable<(string groupName, List<Feature> features)> GetGroups() {
+        foreach (var group in FeatureGroups) {
+            yield return (group.Key, group.Value);
+        }
+    }
     public virtual void InitializeAll() {
         Stopwatch a = Stopwatch.StartNew();
-        foreach (var feature in Features) {
+        foreach (var feature in GetFeatures()) {
             if (feature is not INeedEarlyInitFeature) {
                 feature.Initialize();
             }
@@ -22,13 +37,13 @@ public abstract class FeatureTab {
         Debug($"!!Threaded!!: {GetType().Name} lazy init took {a.ElapsedMilliseconds}ms");
     }
     public virtual void DestroyAll() {
-        foreach (var feature in Features) {
+        foreach (var feature in GetFeatures()) {
             feature.Destroy();
         }
     }
     public virtual void OnGui() {
         Div.DrawDiv();
-        foreach (var feature in Features) {
+        foreach (var feature in GetFeatures()) {
             feature.OnGui();
             Div.DrawDiv();
         }
