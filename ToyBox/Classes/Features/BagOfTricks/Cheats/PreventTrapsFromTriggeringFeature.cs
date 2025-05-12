88d6ec424e8e68e8a9af255da261276ac79095be
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-05-12 20:30:20 +0200

    typo

--- ToyBox/Classes/Features/BagOfTricks/Cheats/PreventTrapsFromTriggeringFeature.cs
+++ ToyBox/Classes/Features/BagOfTricks/Cheats/PreventTrapsFromTriggeringFeature.cs
@@ -8,7 +8,7 @@ public partial class PreventTrapsFromTriggeringFeature : FeatureWithPatch {
     public override ref bool IsEnabled => ref Settings.DisableTraps;
     [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_PreventTrapsFromTriggeringFeature_Name", "Prevent Traps from triggering")]
     public override partial string Name { get; }
-    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_PreventTrapsFromTriggeringFeature_Description", "Enterint a Trap Zone while having Traps disabled will prevent that Trap from triggering even if you deactivate this option in the future")]
+    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_PreventTrapsFromTriggeringFeature_Description", "Entering a Trap Zone while having Traps disabled will prevent that Trap from triggering even if you deactivate this option in the future")]
     public override partial string Description { get; }
 
     [HarmonyPatch(typeof(TrapObjectData), nameof(TrapObjectData.TryTriggerTrap)), HarmonyPrefix]
