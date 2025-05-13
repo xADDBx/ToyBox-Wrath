b4fa1d88321e337412a393cd217f8282ee29045f
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-05-13 19:02:21 +0200

    Increase translation clarity?

--- ToyBox/Classes/Features/BagOfTricks/Cheats/DisablePartyAbilityDamage.cs
+++ ToyBox/Classes/Features/BagOfTricks/Cheats/DisablePartyAbilityDamage.cs
@@ -7,9 +7,9 @@ namespace ToyBox.Features.BagOfTricks.Cheats;
 public partial class DisablePartyAbilityDamage : FeatureWithPatch {
     protected override string HarmonyName => "ToyBox.Features.BagOfTricks.Cheats.DisablePartyAbilityDamage";
     public override ref bool IsEnabled => ref Settings.DisablePartyAbilityDamage;
-    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_DisablePartyAbilityDamage_DisablePartyAbilityDamageText", "Disable Party Ability Damage")]
+    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_DisablePartyAbilityDamage_DisablePartyAbilityDamageText", "Prevent Party abilities from being reduced")]
     public override partial string Name { get; }
-    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_DisablePartyAbilityDamage_PreventPartyMembersFromBeingAffe", "Prevent Party members from being affected by stat reducing attacks")] 
+    [LocalizedString("ToyBox_Features_BagOfTricks_Cheats_DisablePartyAbilityDamage_PreventPartyMembersFromBeingAffe", "Prevents Party members from being affected by stat reducing attacks")] 
     public override partial string Description { get; }
     [HarmonyPatch(typeof(RuleDealStatDamage), nameof(RuleDealStatDamage.Immune), MethodType.Getter), HarmonyPostfix]
     public static void RuleDealStatDamage_Immune_Postfix(RuleDealStatDamage __instance , ref bool __result) {
