f49622ed9577dd191d483f2bf23457c140e22cbd
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-05-27 04:05:58 +0200

    Implement Enum SelectionGrid

---
+++ ToyBox/Classes/Infrastructure/UI/Controls/Grids.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace ToyBox.Infrastructure.UI;
+public static partial class UI {
+    private static Dictionary<Type, Array> m_EnumCache = new();
+    private static Dictionary<Type, Dictionary<object, int>> m_IndexToEnumCache = new();
+    private static Dictionary<Type, string[]> m_EnumNameCache = new();
+    public static bool SelectionGrid<TEnum>(ref TEnum selected, int xCols, Func<TEnum, string>? titler, params GUILayoutOption[] options) where TEnum : Enum {
+        if (!m_EnumCache.TryGetValue(typeof(TEnum), out var vals)) {
+            vals = Enum.GetValues(typeof(TEnum));
+        }
+        if (m_EnumNameCache.TryGetValue(typeof(TEnum), out var names)) {
+            Dictionary<object, int> indexToEnum = new();
+            List<string> tmpNames = new();
+            for (int i = 0; i < vals.Length; i++) {
+                string name;
+                var val = vals.GetValue(i);
+                indexToEnum[val] = i;
+                if (titler != null) {
+                    name = titler((TEnum)val);
+                } else {
+                    name = Enum.GetName(typeof(TEnum), val);
+                }
+                tmpNames.Add(name);
+            }
+            names = [.. tmpNames];
+            m_EnumNameCache[typeof(TEnum)] = names;
+            m_IndexToEnumCache[typeof(TEnum)] = indexToEnum;
+        }
+        if (xCols < 0) {
+            xCols = vals.Length;
+        }
+        var selectedInt = m_IndexToEnumCache[typeof(Enum)][selected];
+        var newSel = GUILayout.SelectionGrid(selectedInt, names, xCols, options);
+        bool changed = selectedInt != newSel;
+        if (changed) {
+            selected = (TEnum)vals.GetValue(newSel);
+        }
+        return changed;
+    }
+}
