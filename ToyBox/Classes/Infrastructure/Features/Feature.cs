f0aab5590268cbf7f2de624d5c577ef003c82ddf
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-06-09 15:54:41 +0200

    System to access Features without much overhead (I could've used a cyclic generic system like Feature<T> where T : Feature<T>, new() and then use the Instance pattern, but that would've meant rewriting a every single class. The current

--- ToyBox/Classes/Infrastructure/Features/Feature.cs
+++ ToyBox/Classes/Infrastructure/Features/Feature.cs
@@ -1,5 +1,20 @@
-﻿namespace ToyBox;
+﻿using System.Collections.Concurrent;
+
+namespace ToyBox;
 public abstract class Feature {
+    private static readonly ConcurrentDictionary<Type, Feature> m_Instances = [];
+    protected Feature() {
+        var t = GetType();
+        if (!m_Instances.TryAdd(t, this)) {
+            throw new InvalidOperationException($"Feature of type {t.Name} was already constructed.");
+        }
+    }
+    public static Feature GetInstance(Type featureType) {
+        if (!m_Instances.TryGetValue(featureType, out var inst)) {
+            inst = (Feature)Activator.CreateInstance(featureType, true);
+        }
+        return m_Instances[featureType] = inst;
+    }
     public abstract string Name { get; }
     public abstract string Description { get; }
     public abstract void OnGui();
