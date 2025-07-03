7be2348a7b2d4e163ad372f39ea2ad3f85b44ebd
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-07-03 02:39:23 +0200

    using

--- ToyBox/Classes/Infrastructure/Update/VersionChecker.cs
+++ ToyBox/Classes/Infrastructure/Update/VersionChecker.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using UnityModManagerNet;
 
 namespace ToyBox {
