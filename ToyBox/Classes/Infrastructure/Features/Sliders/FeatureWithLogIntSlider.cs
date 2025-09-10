377fc0321f8335cbca2106d38d7a1c28b6349490
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-09-11 00:06:46 +0200

    Add LogSliders

---
+++ ToyBox/Classes/Infrastructure/Features/Sliders/FeatureWithLogIntSlider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace ToyBox;
+public abstract class FeatureWithLogIntSlider : Feature {
+    public override void Initialize() {
+        base.Initialize();
+        IsInitialized = true;
+    }
+    public override void Destroy() {
+        base.Destroy();
+        IsInitialized = false;
+    }
+    protected bool IsInitialized = false;
+    public abstract bool IsEnabled { get; }
+    public abstract ref int Value { get; }
+    public abstract int Min { get; }
+    public abstract int Max { get; }
+    public abstract int? Default { get; }
+    protected virtual void OnValueChanged((int oldValue, int newValue) vals) {
+        if (IsEnabled) {
+            if (!IsInitialized) {
+                Initialize();
+            }
+        } else {
+            if (IsInitialized) {
+                Destroy();
+            }
+        }
+    }
+    public override void OnGui() {
+        using (HorizontalScope()) {
+            UI.LogSlider(ref Value, Min, Max, Default, OnValueChanged, AutoWidth(), GUILayout.MinWidth(50), GUILayout.MaxWidth(150));
+            Space(10);
+            UI.Label(Name.Cyan());
+            Space(10);
+            UI.Label(Description.Green());
+        }
+    }
+}
--- ToyBox/Classes/Infrastructure/UI/Controls/Sliders.cs
+++ ToyBox/Classes/Infrastructure/UI/Controls/Sliders.cs
@@ -38,4 +38,54 @@ public static partial class UI {
         }
         return false;
     }
+    public static bool LogSlider(ref int value, float minValue, float maxValue, int? defaultValue = null, Action<(int oldValue, int newValue)>? onValueChanged = null, params GUILayoutOption[] options) {
+        options = options.Length == 0 ? [AutoWidth(), Width(600)] : options;
+        var oldValue = value;
+        const int offset = 1;
+
+        float logValue = 100f * (float)Math.Log10(value + offset);
+        float logMin = 100f * (float)Math.Log10(minValue + offset);
+        float logMax = 100f * (float)Math.Log10(maxValue + offset);
+
+        float logResult = GUILayout.HorizontalSlider(logValue, logMin, logMax, options);
+        int result = (int)Math.Round(Math.Pow(10, logResult / 100f) - offset, 0);
+        Label(value.ToString().Orange() + " ");
+        if (defaultValue != null) {
+            Space(4);
+            Button(SharedStrings.ResetToDefault, () => {
+                result = defaultValue.Value;
+            });
+        }
+        if (Math.Abs((result - value)) > float.Epsilon) {
+            value = result;
+            onValueChanged?.Invoke((oldValue, value));
+            return true;
+        }
+        return false;
+    }
+    public static bool LogSlider(ref float value, float minValue, float maxValue, float? defaultValue = null, int digits = 2, Action<(float oldValue, float newValue)>? onValueChanged = null, params GUILayoutOption[] options) {
+        options = options.Length == 0 ? [AutoWidth(), Width(600)] : options;
+        var oldValue = value;
+        const int offset = 1;
+
+        float logValue = 100f * (float)Math.Log10(value + offset);
+        float logMin = 100f * (float)Math.Log10(minValue + offset);
+        float logMax = 100f * (float)Math.Log10(maxValue + offset);
+
+        float logResult = GUILayout.HorizontalSlider(logValue, logMin, logMax, options);
+        float result = (float)Math.Round(Math.Pow(10, logResult / 100f) - offset, digits);
+        Label(value.ToString().Orange() + " ");
+        if (defaultValue != null) {
+            Space(4);
+            Button(SharedStrings.ResetToDefault, () => {
+                result = defaultValue.Value;
+            });
+        }
+        if (Math.Abs((result - value)) > float.Epsilon) {
+            value = result;
+            onValueChanged?.Invoke((oldValue, value));
+            return true;
+        }
+        return false;
+    }
 }
