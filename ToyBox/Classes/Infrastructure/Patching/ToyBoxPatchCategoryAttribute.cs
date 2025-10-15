039d58d39611d35df1c8e401c4359208e1ff999e
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-10-16 00:10:41 +0200

    .

--- ToyBox/Classes/Infrastructure/Patching/ToyBoxPatchCategoryAttribute.cs
+++ ToyBox/Classes/Infrastructure/Patching/ToyBoxPatchCategoryAttribute.cs
@@ -9,7 +9,9 @@ public class ToyBoxPatchCategoryAttribute : Attribute {
         Category = category;
     }
     public static void PatchCategory(string categoryName, Harmony harmony) {
-        if (m_HarmonyCategoryCache == null) CreateHarmonyCategoryCache();
+        if (m_HarmonyCategoryCache == null) {
+            CreateHarmonyCategoryCache();
+        }
         if (m_HarmonyCategoryCache!.TryGetValue(categoryName, out var toPatch)) {
             try {
                 toPatch.Do(type => {
@@ -22,14 +24,14 @@ public class ToyBoxPatchCategoryAttribute : Attribute {
         }
     }
     public static void CreateHarmonyCategoryCache() {
-        m_HarmonyCategoryCache = new();
+        m_HarmonyCategoryCache = [];
         foreach (var type in AccessTools.GetTypesFromAssembly(typeof(FeatureWithPatch).Assembly)) {
             List<HarmonyMethod> fromType = HarmonyMethodExtensions.GetFromType(type);
             HarmonyMethod harmonyMethod = HarmonyMethod.Merge(fromType);
             var attr = type.GetCustomAttribute<ToyBoxPatchCategoryAttribute>();
             if (!string.IsNullOrEmpty(attr?.Category)) {
                 if (!m_HarmonyCategoryCache.TryGetValue(attr!.Category, out var typeList)) {
-                    typeList ??= new();
+                    typeList ??= [];
                 }
                 typeList.Add(type);
                 m_HarmonyCategoryCache[attr.Category] = typeList;
